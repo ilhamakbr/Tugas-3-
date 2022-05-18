c437145b1bd879fff9bbe4b1f7874e4b122b326c
ilhamakbr <102602156+ilhamakbr@users.noreply.github.com>
2022-05-18 20:37:19 +0700

    Create Latihan 1.php

---
+++ Latihan 1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<head>
+      <title>HTML Frames</title>
+   </head>
+   
+   
+   <frameset cols = "25%,75%">
+      <frame name = "left" src = "main.html" />
+      <frame name = "center" src = "tengah.php" />
+    
+      
+      <noframes>
+         <body>Your browser does not support frames.</body>
+      </noframes>
+   </frameset>
+</head>
+<body>
+
+</body>
+</html>
